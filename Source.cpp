1afa20ff922a0f538d7fbb4ff3b22b1b7f527a7c
Tharusha Kudagala <44969527+tharusha-kudagala@users.noreply.github.com>
2019-10-15 09:46:33 +0530

    Added Spanish

--- Source.cpp
+++ Source.cpp
@@ -1,12 +1,13 @@
 #include<iostream>
 int main() {
 	using std::cout;
-	cout << "Display Hello in five languages\n";
+	cout << "Display Hello in six languages\n";
 	cout << "1. English - Hello\n";
 	cout << "2. Sinhala - Ayubowan\n";
 	cout << "3. Malay - Salamat\n";
 	cout << "4. French - Bonjour\n";
 	cout << "5. Hindi - Namaste\n";
+	cout << "6. Spanish - Hola\n";
 	system("pause");
 	return 0;
 }
